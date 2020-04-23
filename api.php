de597664a5f27461f0e09359ded799dfd2409c7e
MartinVecera <36948869+MartinVecera@users.noreply.github.com>
2020-04-23 20:56:41 +0200

    upravy

--- api.php
+++ api.php
@@ -1,5 +1,23 @@
-<?php
+<?php 
 
+include("tridy.php");
 
+$infikovani = new Covid();
 
 ?>
+
+<!DOCTYPE html>
+<html lang="cs">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<p><?php echo $infikovani->celkem() ?></p>
+
+
+</body>
+</html>
